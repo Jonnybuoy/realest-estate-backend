3ce7248ebe61dfc2603e105f69e004b08bd69d94
Jonnybuoy <kaguithiajohnson@gmail.com>
2025-01-29 15:57:11 +0300

    Set up AWS S3

--- realest_estate/settings.py
+++ realest_estate/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'realtors',
     'listings',
     'contacts',
+
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -183,3 +185,17 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+
+# AWS S3 CONFIGURATION
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+# Media Files Storage
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Set the URL for accessing media files
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
